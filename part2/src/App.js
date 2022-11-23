4f50e2c0b50e3f0b0acc35107fd10c4e02a66ea4
KowshikI87 <kowshikislam8@gmail.com>
2022-11-23 02:19:12 -0500

    2b done with exercise

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,55 +1,121 @@
 import { useState } from 'react'
-import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
-
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
-    }
-  
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-  }
-
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
-
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important)
-
+const AddNewMember = ( {newName, newPhoneNumber,
+  handleNameChange, handleAddName, handlePhoneNumberChange} ) => {
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
-        </button>
-      </div>
-      <ul>
-        {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>   
+      <h2>add a new </h2>
+      <form onSubmit={handleAddName}>
+        <div>
+          <p>name:
+          <input 
+            value={newName}
+            onChange={handleNameChange}
+          /></p>
+          <p>phone number:
+          <input 
+            value={newPhoneNumber}
+            onChange={handlePhoneNumberChange}
+          /></p>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
     </div>
   )
 }
 
-export default App 
+const Numbers = ( {persons, nameFilter} ) => {
+  let filteredPersons
+  if (nameFilter.length === 0) {
+    filteredPersons = persons;
+  } else {
+    filteredPersons = persons.filter(person => {
+      let lowerCaseNameFilter = nameFilter.toLowerCase()
+      let lowerCaseName = person.name.toLowerCase();
+      return lowerCaseName.startsWith(lowerCaseNameFilter);
+    })
+  }
+
+  return (
+    <div>
+      <h2>Numbers</h2>
+      {filteredPersons.map((person, idx) => 
+        <Member key={idx} name={person.name} phoneNumber={person.phoneNumber}/>
+      )}
+    </div>
+  )
+}
+
+const Member = ( {name, phoneNumber} ) => {
+  return (
+    <p>
+      {name} {phoneNumber}
+    </p>
+  )
+}
+
+const NameFilter = ( {newNameFilter, handleNameFilterChange} ) => {
+  return (
+    <div>
+      <form>
+        <p>filter shown with
+          <input
+          value={newNameFilter}
+          onChange={handleNameFilterChange}>
+          </input>
+        </p>
+      </form>
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newPhoneNumber, setNewPhoneNumber] = useState('')
+  const [newNameFilter, setNewNameFilter] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handlePhoneNumberChange = (event) => {
+    setNewPhoneNumber(event.target.value)
+  }
+
+  const handleNameFilterChange = (event) => {
+    setNewNameFilter(event.target.value)
+  }
+
+  const handleAddName = (event) => {
+    event.preventDefault();
+    let isCurntNameExist = persons.some(person => {
+      return person.name === newName
+    })
+
+    if (isCurntNameExist) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat({name: newName, phoneNumber: newPhoneNumber}))
+    }
+  }
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+
+      <NameFilter newNameFilter={newNameFilter} handleNameFilterChange={handleNameFilterChange} />
+
+      <AddNewMember newName={newName} newPhoneNumber={newPhoneNumber}
+      handleNameChange={handleNameChange} handleAddName={handleAddName} 
+      handlePhoneNumberChange={handlePhoneNumberChange} />
+
+      <Numbers persons={persons} nameFilter={newNameFilter} />
+
+    </div>
+  )
+}
+
+export default App
