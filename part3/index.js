95e98980ccc570ccd5540198237c1613a60e1172
Kowshik Islam <kowshikislam8@gmail.com>
2022-11-28 03:42:58 -0500

    working through mongodb

--- part3/index.js
+++ part3/index.js
@@ -4,32 +4,6 @@ const cors = require('cors')
 const app = express()
 const Note = require('./models/note')
 
-
-/*
-const mongoose = require('mongoose')
-
-const url = `mongodb+srv://kowshikI87:8Zc4we6g376mnblr@cluster0.fe0tjdr.mongodb.net/noteApp?retryWrites=true&w=majority`
-
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-*/
-
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
@@ -43,21 +17,8 @@ app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes)
   })
-  // response.json(notes)
 })
 
-/*
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-})
-*/
 
 app.get('/api/notes/:id', (request, response) => {
   Note.findById(request.params.id).then(note => {
@@ -65,43 +26,31 @@ app.get('/api/notes/:id', (request, response) => {
   })
 })
 
+//did it on my own
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+  Note.findById(request.params.id).then(note => {
+    note.deleteOne().then(() => {
+      response.status(204).end()
+    })
+  })
 })
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  }
+  })
 
   note.save().then(savedNote => {
     response.json(savedNote)
   })
-
-  // notes = notes.concat(note)
-
-  // response.json(note)
 })
 
 const PORT = process.env.PORT
