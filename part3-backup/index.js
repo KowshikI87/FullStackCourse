ff1b1b333ce343b9ece3e06e0d8bfa9dcae12c87
Kowshik Islam <kowshikislam8@gmail.com>
2022-11-28 02:35:43 -0500

    working through mongodb deployment

--- part3-backup/index.js
+++ part3-backup/index.js
@@ -1,52 +1,40 @@
-require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const Note = require('./models/note')
-
-
-/*
-const mongoose = require('mongoose')
-
-const url = `mongodb+srv://kowshikI87:8Zc4we6g376mnblr@cluster0.fe0tjdr.mongodb.net/noteApp?retryWrites=true&w=majority`
-
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-*/
 
 app.use(express.json())
-app.use(cors())
-app.use(express.static('build'))
 
+app.use(cors())
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2022-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2022-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2022-05-30T19:20:14.298Z",
+    important: true
+  }
+]
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-  // response.json(notes)
+  response.json(notes)
 })
 
-/*
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
@@ -56,13 +44,6 @@ app.get('/api/notes/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
-})
-*/
-
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -95,16 +76,12 @@ app.post('/api/notes', (request, response) => {
     id: generateId(),
   }
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  notes = notes.concat(note)
 
-  // notes = notes.concat(note)
-
-  // response.json(note)
+  response.json(note)
 })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
