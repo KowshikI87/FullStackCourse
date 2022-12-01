7f117e47459bd9ad8e3187060685596a762a830b
KowshikI87 <kowshikislam8@gmail.com>
2022-11-30 23:41:58 -0500

    Part 3c done without exercise

--- part3/index.js
+++ part3/index.js
@@ -19,20 +19,34 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-//did it on my own
-app.delete('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    note.deleteOne().then(() => {
+// //did it on my own
+// app.delete('/api/notes/:id', (request, response) => {
+//   Note.findById(request.params.id).then(note => {
+//     note.deleteOne().then(() => {
+//       response.status(204).end()
+//     })
+//   })
+// })
+
+//solution from course for deleting a note
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
       response.status(204).end()
     })
-  })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -52,6 +66,35 @@ app.post('/api/notes', (request, response) => {
     response.json(savedNote)
   })
 })
+
+//toggling importance
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
