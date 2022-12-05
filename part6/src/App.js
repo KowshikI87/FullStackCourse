6b230a10e2286ca3436955628e3bd0e66292ec34
KowshikI87 <kowshikislam8@gmail.com>
2022-12-05 10:22:15 -0500

    6a counter app done

---
+++ part6/src/App.js
@@ -0,0 +1,87 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
+import noteService from './services/notes'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    //changed note is old note copied but "important" boolean variable
+    //is toggled
+    const changedNote = { ...note, important: !note.important }
+
+      //once the request is successful, we update newNote by calling setNotes
+    noteService
+    .update(id, changedNote)
+    .then(returnedNote => {
+      //we pass an array where all the other note objects apart from
+      //the note object changed is copied; for the note object changed, we copy
+      //it from response.data (recall that it only returns the object we changed)
+      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+    })
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>   
+      <ul>
+        {notesToShow.map(note => 
+          <Note 
+            key={note.id} 
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)}/>
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
