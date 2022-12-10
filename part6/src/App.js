6f7c5f3289e6d7f1dc1150fb69f1ed5c3144df1c
KowshikI87 <kowshikislam8@gmail.com>
2022-12-09 23:54:34 -0500

    commit before massive changes to app.js file in 6a

--- part6/src/App.js
+++ part6/src/App.js
@@ -1,85 +1,57 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import Note from './components/Note'
-import noteService from './services/notes'
+import { createStore } from 'redux'
+import noteReducer from './reducers/noteReducer'
+
+const store = createStore(noteReducer)
+
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
+
+const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    data: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    data: { id }
+  }
+}
 
 const App = () => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
-
-  useEffect(() => {
-    noteService
-      .getAll()
-      .then(initialNotes => {
-        setNotes(initialNotes)
-      })
-  }, [])
-
   const addNote = (event) => {
     event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() > 0.5
-    }
-
-    noteService
-      .create(noteObject)
-      .then(returnedNote => {
-        setNotes(notes.concat(returnedNote))
-        setNewNote('')
-      })
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch(createNote(content))
   }
 
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value)
-  }
-
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important)
-
-  const toggleImportanceOf = id => {
-    const note = notes.find(n => n.id === id)
-    //changed note is old note copied but "important" boolean variable
-    //is toggled
-    const changedNote = { ...note, important: !note.important }
-
-      //once the request is successful, we update newNote by calling setNotes
-    noteService
-    .update(id, changedNote)
-    .then(returnedNote => {
-      //we pass an array where all the other note objects apart from
-      //the note object changed is copied; for the note object changed, we copy
-      //it from response.data (recall that it only returns the object we changed)
-      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-    })
+  const toggleImportance = (id) => {
+    store.dispatch(toggleImportanceOf(id))
   }
 
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
-        </button>
-      </div>   
+      <form onSubmit={addNote}>
+        <input name="note" /> 
+        <button type="submit">add</button>
+      </form>
       <ul>
-        {notesToShow.map(note => 
-          <Note 
+        {store.getState().map(note =>
+          <li
             key={note.id} 
-            note={note} 
-            toggleImportance={() => toggleImportanceOf(note.id)}/>
+            onClick={() => toggleImportance(note.id)}
+          >
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
         )}
       </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>
     </div>
   )
 }
