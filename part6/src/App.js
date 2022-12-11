fe23b9362a942c6da2f1986cc1cabeec370913b8
KowshikI87 <kowshikislam8@gmail.com>
2022-12-11 16:06:35 -0500

    Finish Part 6c without exercises

--- part6/src/App.js
+++ part6/src/App.js
@@ -2,19 +2,15 @@ import { useEffect } from 'react'
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
-import noteService from './services/notes'
-import { setNotes } from './reducers/noteReducer'
+import { initializeNotes } from './reducers/noteReducer'
+
 import { useDispatch } from 'react-redux'
 
 
-//Q: I am not sure how many times this useeffect is executed
-//considering that every time we rerender App component, it should rerun
-//checck notes on useEffect
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    noteService
-      .getAll().then(notes => dispatch(setNotes(notes)))
+    dispatch(initializeNotes()) 
   }, [dispatch])
 
   return (
